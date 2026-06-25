6831a21040ed9c8790c6d22711503f9d931f9c8f
吴语萌 <mengbad@qq.com>
2026-06-26 06:26:47 +0800

    remove obsolete minimal.mk inheritance

--- twrp_dada.mk
+++ twrp_dada.mk
@@ -1,6 +1,5 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/minimal.mk)
 
 # Inherit from our custom TWRP product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
