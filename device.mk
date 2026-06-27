5134242d3451b78ba1bec2aa632a6f97b058c6bf
吴语萌 <mengbad@qq.com>
2026-06-27 17:58:31 +0800

    fix kernel base offset, add super partition parameters, and FBE v2 decryption options

--- device.mk
+++ device.mk
@@ -9,3 +9,9 @@ AB_OTA_UPDATER := true
 
 # Dynamic partitions support
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Override version properties for KeyStore/KeyMint
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.build.version.release=15 \
+    ro.build.version.release_or_codename=15 \
+    ro.build.version.sdk=35
