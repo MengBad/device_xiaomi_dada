9cdce07f61c3c89bd96bd3f4d159eca9cd3027d4
吴语萌 <mengbad@qq.com>
2026-06-27 18:12:28 +0800

    revert TARGET_CPU_VARIANT to generic to fix build failure on older AOSP branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ DEVICE_PATH := device/xiaomi/dada
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-x4
+TARGET_CPU_VARIANT := generic
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := dada
